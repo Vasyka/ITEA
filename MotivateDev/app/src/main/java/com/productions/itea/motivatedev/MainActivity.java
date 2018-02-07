7ffc3d24c4fdbdd41a5adfa1fbfe3abc604d5ec5
Vasyka <vvanast@yandex.ru>
2018-02-07 08:33:57 +0300

    Auth bugs were fixed
    Auth bugs were fixed + better decomposition

--- MotivateDev/app/src/main/java/com/productions/itea/motivatedev/MainActivity.java
+++ MotivateDev/app/src/main/java/com/productions/itea/motivatedev/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import com.firebase.ui.auth.AuthUI;
@@ -18,23 +17,28 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 
 import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
-
     // Choose an arbitrary request code value
     private static final int RC_SIGN_IN = 123;
+
+    private static final String TAG = "MainActivity";
 
     // Firebase instance variables
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
 
+    public static Intent createIntent(Context context) {
+        return new Intent(context, SignInActivity.class);
+    }
+
+    private void showSnackbar(int id) {
+        Snackbar.make(findViewById(R.id.container), getResources().getString(id), Snackbar.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,76 +65,13 @@ public class MainActivity extends AppCompatActivity {
             // FirebasemUser.getToken() instead.
             String uid = mUser.getUid();
 
-            /* ------- Update user's profile ---------*/
-            /*
-            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                    .setDisplayName("Jane Q. User")
-                    .setPhotoUri(Uri.parse("https://example.com/jane-q-user/profile.jpg"))
-                    .build();
 
-            mUser.updateProfile(profileUpdates)
-                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                Log.d(TAG, "User profile updated.");
-                            }
-                        }
-                    });*/
         } else {
 
-            startActivityForResult(
-                    // Get an instance of AuthUI based on the default app
-                    AuthUI.getInstance().createSignInIntentBuilder()
-                    .setAvailableProviders(Arrays.asList(
-                            new AuthUI.IdpConfig.EmailBuilder().build(),
-                            new AuthUI.IdpConfig.GoogleBuilder().build()))
-                            .setIsSmartLockEnabled(false).build(), RC_SIGN_IN);
-
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
         }
         setContentView(R.layout.activity_main);
-    }
-
-    public static Intent createIntent(Context context) {
-        return new Intent(context, MainActivity.class);
-    }
-
-    private void showSnackbar(int id) {
-        Snackbar.make(findViewById(R.id.container), getResources().getString(id), Snackbar.LENGTH_LONG).show();
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
-        if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
-
-            // Successfully signed in
-            if (resultCode == RESULT_OK) {
-                startActivity(SignedInActivity.createIntent(this)); //response
-                finish();
-                return;
-            } else {
-                // Sign in failed
-                if (response == null) {
-                    // User pressed back button
-                    showSnackbar(R.string.sign_in_cancelled);
-                    return;
-                }
-
-                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
-                    showSnackbar(R.string.no_internet_connection);
-                    return;
-                }
-
-                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
-                    showSnackbar(R.string.unknown_error);
-                    return;
-                }
-            }
-
-            showSnackbar(R.string.unknown_sign_in_response);
-        }
     }
 
     public void signOut(View view) {
