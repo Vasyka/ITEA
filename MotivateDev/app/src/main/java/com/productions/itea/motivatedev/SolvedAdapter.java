a3d2278725a69ab8eac535ce80a1e5f73a2b674e
Vesanka <aleks_shapiro@mail.ru>
2018-02-15 02:55:00 +0300

    Solved tasks works

---
+++ MotivateDev/app/src/main/java/com/productions/itea/motivatedev/SolvedAdapter.java
@@ -0,0 +1,160 @@
+package com.productions.itea.motivatedev;
+
+
+import android.content.Context;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SolvedAdapter extends RecyclerView.Adapter<SolvedAdapter.SolvedViewHolder> {
+
+    private static final String TAG = "MainActivity";
+    static class SolvedViewHolder extends RecyclerView.ViewHolder {
+
+        TextView solvedView;
+        public ImageButton menuImageButton;
+
+
+        SolvedViewHolder(View itemView) {
+            super(itemView);
+            solvedView = itemView.findViewById(R.id.my_text_view_solved);
+            menuImageButton = itemView.findViewById((R.id.task_menu));
+        }
+    }
+
+
+
+    private Context mContext;
+    private DatabaseReference mRef;
+
+    private List<String> solvedIds = new ArrayList<>();
+    private List<myTask> mySolved = new ArrayList<>();
+
+
+    public SolvedAdapter(Context context, DatabaseReference solvedRef) {
+
+        mContext = context;
+        mRef = solvedRef;
+
+
+        ChildEventListener childEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+                Log.d(TAG, "onChildAdded:" + dataSnapshot.getKey());
+
+                // A new task has been added, add it to the displayed list
+                myTask mytask = dataSnapshot.getValue(myTask.class);
+
+                // Update
+                solvedIds.add(dataSnapshot.getKey());
+                mySolved.add(mytask);
+
+                //Update Recycleview
+                notifyItemInserted(mySolved.size() - 1);
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName) {
+                Log.d(TAG, "onChildChanged:" + dataSnapshot.getKey());
+
+                // A task has changed, use the key to determine if we are displaying this
+                // task and if so displayed the changed task.
+                myTask mytask = dataSnapshot.getValue(myTask.class);
+
+                String taskKey = dataSnapshot.getKey();
+
+                int taskIndex = solvedIds.indexOf(taskKey);
+                if (taskIndex > -1) {
+                    // Replace with the new data
+                    mySolved.set(taskIndex, mytask);
+
+                    //Update Recycleview
+                    notifyItemChanged(taskIndex);
+
+                } else {
+                    Log.w(TAG, "onChildChanged:unknown_child:" + taskKey);
+                }
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                Log.d(TAG, "onChildRemoved:" + dataSnapshot.getKey());
+
+                // A task has changed, use the key to determine if we are displaying this
+                // task and if so remove it.
+                String taskKey = dataSnapshot.getKey();
+
+                int taskIndex = solvedIds.indexOf(taskKey);
+                if (taskIndex > -1) {
+                    // Remove data from the list
+                    mySolved.remove(taskIndex);
+                    solvedIds.remove(taskIndex);
+
+                    //Update Recycleview
+                    notifyItemRemoved(taskIndex);
+
+                } else {
+                    Log.w(TAG, "onChildChanged:unknown_child:" + taskKey);
+                }
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
+                Log.d(TAG, "onChildMoved:" + dataSnapshot.getKey());
+
+                // A task has changed, use the key to determine if we are displaying this
+                // task and if so move it.
+                myTask mytask = dataSnapshot.getValue(myTask.class);
+                String taskKey = dataSnapshot.getKey();
+
+                // ...
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "Tasks:onCancelled", databaseError.toException());
+                Toast.makeText(mContext, "Failed to load tasks.", Toast.LENGTH_SHORT).show();
+            }
+        };
+        solvedRef.addChildEventListener(childEventListener);
+
+
+    }
+
+    @Override
+    public SolvedViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View view = inflater.inflate(R.layout.solved_layout, parent, false);
+
+        // set the view's size, margins, paddings and layout parameters
+
+        return new SolvedAdapter.SolvedViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(SolvedViewHolder holder, int position) {
+        holder.solvedView.setText(mySolved.get(position).task_name);
+
+    }
+
+    @Override
+    public int getItemCount() {
+      return mySolved.size();
+    }
+
+
+
+}
--- MotivateDev/app/src/main/java/com/productions/itea/motivatedev/SolvedTasksFragment.java
+++ MotivateDev/app/src/main/java/com/productions/itea/motivatedev/SolvedTasksFragment.java
@@ -1,13 +1,30 @@
 package com.productions.itea.motivatedev;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class SolvedTasksFragment extends Fragment {
+
+
+
+    public RecyclerView mRecyclerView;
+
+    private FirebaseDatabase myDb;
+    public SolvedAdapter solvedAdapter;
+
 
     public SolvedTasksFragment() {
         // Required empty public constructor
@@ -23,31 +40,42 @@ public class SolvedTasksFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_solved_tasks, container, false);
+        View v = inflater.inflate(R.layout.fragment_solved_tasks, container, false);
+        // Firebase Auth
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser curUser = mAuth.getCurrentUser();
+        if (curUser != null){
+
+            mRecyclerView = (RecyclerView) v.findViewById(R.id.solved_rec);
+            mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+            String uid = curUser.getUid();
+            myDb = FirebaseDatabase.getInstance();
+            Log.d("DBDBDBDBD",myDb != null ? "OK" : "Oops" );
+            DatabaseReference solvedTasksRef = myDb.getReference("solved_tasks").child(uid);
+            solvedAdapter = new SolvedAdapter(getActivity(), solvedTasksRef);
+            mRecyclerView.setAdapter(solvedAdapter);
+
+        }
+
+
+
+        return v;
     }
 
 
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnSolvedTasksInteractionListener) {
-//            mListener = (OnSolvedTasksInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnSolvedTasksInteractionListener");
-//        }
-//    }
-
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
 
 
-//    public interface OnSolvedTasksInteractionListener {
-//        // TODO: Update argument type and name
-//        void onFragmentInteraction(Uri uri);
-//    }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+
+
 }
