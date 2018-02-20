bdaa0bd2daa5ef16b1f3bf3144fe4cd123820855
Vesanka <aleks_shapiro@mail.ru>
2018-02-20 04:14:28 +0300

    Code for toolbar in rating activity

--- MotivateDev/app/src/main/java/com/productions/itea/motivatedev/RatingActivity.java
+++ MotivateDev/app/src/main/java/com/productions/itea/motivatedev/RatingActivity.java
@@ -1,11 +1,20 @@
 package com.productions.itea.motivatedev;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -25,6 +34,11 @@ public class RatingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
+        //Toolbar
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.inflateMenu(R.menu.toolbar_menu);
+
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         FirebaseUser curUser = mAuth.getCurrentUser();
         if (curUser != null) {
@@ -39,6 +53,41 @@ public class RatingActivity extends AppCompatActivity {
             DatabaseReference ratingRef = myDb.getReference("rating").child(group_id);
             ratingAdapter = new RatingAdapter(this, ratingRef);
             mRecyclerView.setAdapter(ratingAdapter);
+        }
+    }
+
+    public void signOut() {
+        AuthUI.getInstance().signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(RatingActivity.this, SignInActivity.class));
+                            finish();
+                        } else {
+                            Snackbar.make(findViewById(R.id.container), getResources().
+                                    getString(R.string.sign_out_failed), Snackbar.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.exit_menu:
+                signOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
